b309a9eddfe3b16431d58ebe97a5479ca9f22572
Skyscraper528 <651903069@qq.com>
2017-05-01 23:36:22 +0800

    程序初步

---
+++ app/src/main/java/com/jwei/mysearch/adapter/GoodsAdapter.java
@@ -0,0 +1,41 @@
+package com.jwei.mysearch;
+
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by chen on 2016/12/19.
+ */
+
+public class GoodsAdapter extends ArrayAdapter<Goods>
+{
+    private int resourceId;
+    public GoodsAdapter(Context context, int textViewResourceId, List<Goods>objects)
+    {
+        super(context,textViewResourceId,objects);
+        resourceId=textViewResourceId;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+//        Goods goods= getItem(position);
+//        View view= LayoutInflater.from(getContext()).inflate(resourceId,null);
+//        ImageView goodsimage= (ImageView)view.findViewById(R.id.goods_image);
+//        TextView goodsname=(TextView)view.findViewById(R.id.goods_name);
+//        goodsimage.setImageResource(goods.getGoods_imageid());
+//        goodsname.setText(goods.getGoods_name());
+          return convertView;
+    }
+}
+
